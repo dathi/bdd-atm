5d5a35737e6dd73a1f8b17d8ce9ea37f2eb4cd58
dathi <dathi@tma.com.vn>
2015-06-04 23:30:45 +0700

    change implementation the first step for make sense

--- features/definition_steps/cash_withdrawal_steps.rb
+++ features/definition_steps/cash_withdrawal_steps.rb
@@ -1,11 +1,12 @@
 class Account
-  def initialize(amount)
+  def deposit(amount)
   	
   end
 end
 
 Given(/^I have deposited \$(\d+) in my account$/) do |amount|
-  Account.new(amount.to_i)
+  my_account = Account.new
+  my_account.deposit(amount.to_i)
 end
 
 When(/^I request \$(\d+)$/) do |amount|
